7de76f25463fbd3a942d3a86bd7a5223259a12d3
malory171 <malory171@gmail.com>
2024-08-20 22:09:47 +0200

    Injected provided weather Icon in app.js. Updated index.html. Worked on css

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ function resfreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-  console.log(response.data);
-
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weatherAppIcon"/>`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
